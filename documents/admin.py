7b0974bada6e7dd7aab75464ffc51ed03a73c228
induwari senewirathne <upamalisenewirathne@gmail.com>
2026-04-17 19:40:12 +0530

    Models registered in admin and verified on AWS

--- documents/admin.py
+++ documents/admin.py
@@ -1,3 +1,9 @@
+# documents/admin.py
 from django.contrib import admin
+from .models import Document, DocumentType, ManualUploadDocument, CSVImportLog
 
-# Register your models here.
+# This makes your tables visible in the Django admin panel
+admin.site.register(Document)
+admin.site.register(DocumentType)
+admin.site.register(ManualUploadDocument)
+admin.site.register(CSVImportLog)
