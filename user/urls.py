e48d6d3ea4e76f0d56d942baf4c7d211f81bd872
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-04-24 02:57:51 +0530

    change department endpoints paths

--- user/urls.py
+++ user/urls.py
@@ -9,12 +9,12 @@ urlpatterns = [
     path('get-roles/', GetAllRolesView.as_view(), name='get-roles'),
     path('update-role/<pk>/', UpdateRoleView.as_view(), name='update-role'),
     path('delete-role/<pk>/', DeleteRoleView.as_view(), name='delete-role'),
-    path('save-department/', CreateDepartmentView.as_view(), name='save-department'),
-    path('get-all-departments/', GetDepartmentsView.as_view(), name='get-all-departments'),
-    path('update-department/<pk>/', UpdateDepartmentView.as_view(), name='update-department'),
-    path('delete-department/<pk>/', DeleteDepartmentView.as_view(), name='delete-department'),
+    path('department/save/', CreateDepartmentView.as_view(), name='save-department'),
+    path('department/getAll/', GetDepartmentsView.as_view(), name='get-all-departments'),
+    path('department/update/<pk>/', UpdateDepartmentView.as_view(), name='update-department'),
+    path('department/delete/<pk>/', DeleteDepartmentView.as_view(), name='delete-department'),
     path('save-user/',CreateUserView.as_view(),name='save-user'),
     path('update-user/<pk>/',UpdateUserView.as_view(),name='update-user'),
     path('get-users/',GetUsersView.as_view(),name='get-user'),
-    path('delete-user/<uuid:pk>/',DeleteUserView.as_view(),name='delete-user'),
+    path('delete-user/<uuid:pk>/',DeleteUserView.as_view(),name='delete-user')
 ]
