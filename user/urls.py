8d872bf9cbc4b54bd996e19a39cadb0d630f23d0
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-04-28 23:40:48 +0530

    change url pattern of endpoints

--- user/urls.py
+++ user/urls.py
@@ -4,17 +4,17 @@ from .views import GetAllPermissionsView, SaveRoleView, GetAllRolesView, CreateU
     UpdateDepartmentView, DeleteDepartmentView
 
 urlpatterns = [
-    path('permissions/', GetAllPermissionsView.as_view(), name='get-permissions'),
-    path('save-role/', SaveRoleView.as_view(), name='save-role'),
-    path('get-roles/', GetAllRolesView.as_view(), name='get-roles'),
-    path('update-role/<pk>/', UpdateRoleView.as_view(), name='update-role'),
-    path('delete-role/<pk>/', DeleteRoleView.as_view(), name='delete-role'),
+    path('permission/getAll/', GetAllPermissionsView.as_view(), name='get-permissions'),
+    path('role/save/', SaveRoleView.as_view(), name='save-role'),
+    path('role/getAll/', GetAllRolesView.as_view(), name='get-roles'),
+    path('role/update/<pk>/', UpdateRoleView.as_view(), name='update-role'),
+    path('role/delete/<pk>/', DeleteRoleView.as_view(), name='delete-role'),
     path('department/save/', CreateDepartmentView.as_view(), name='save-department'),
     path('department/getAll/', GetDepartmentsView.as_view(), name='get-all-departments'),
     path('department/update/<pk>/', UpdateDepartmentView.as_view(), name='update-department'),
     path('department/delete/<pk>/', DeleteDepartmentView.as_view(), name='delete-department'),
-    path('save-user/',CreateUserView.as_view(),name='save-user'),
-    path('update-user/<pk>/',UpdateUserView.as_view(),name='update-user'),
-    path('get-users/',GetUsersView.as_view(),name='get-user'),
-    path('delete-user/<uuid:pk>/',DeleteUserView.as_view(),name='delete-user')
+    path('user/save',CreateUserView.as_view(),name='save-user'),
+    path('user/update/<pk>/',UpdateUserView.as_view(),name='update-user'),
+    path('user/getAll/',GetUsersView.as_view(),name='get-user'),
+    path('user/delete/<uuid:pk>/',DeleteUserView.as_view(),name='delete-user')
 ]
