4cfb6577a4b60384673e3a81acfb405ee8c53ba3
sawaniThiwandika <thiwandika.whs02@gmail.com>
2026-05-10 01:17:35 +0530

    add url patters for dashboard app endpoints

---
+++ dashboard/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from dashboard.views import GetAccessibleWidgets, SaveDashboard, GetDashboardList, GetDashboard, UpdateDashboard
+
+urlpatterns = [
+    path('getAccessibleWidgets/<role_id>/',GetAccessibleWidgets.as_view(),name='getAccessibleWidgets'),
+    path('saveDashboard/',SaveDashboard.as_view(),name='saveDashboard'),
+    path('updateDashboard/<id>/',UpdateDashboard.as_view(),name='updateDashboard'),
+    path('getAllDashboard/',GetDashboardList.as_view(),name='getAllDashboard'),
+    path('getDashboard/<dashboard_id>/',GetDashboard.as_view(),name='getDashboard'),
+]
